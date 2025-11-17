cfe1f567a8e6d0e6c2d35e3ae9156a6864130b90
Malinda <greshanamarakoon@gmail.com>
2025-11-17 13:26:32 +0530

    section_10
    Data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,5 @@
 // 9. Values and Variables
+/*
 let js = "amazing";
 console.log(40 + 20 + 10);
 
@@ -9,9 +10,10 @@ let firstname = "Malinda";
 let lastname = "Amarakoon";
 
 console.log(firstname + " " + lastname);
+*/
 
 ///// 10. Practice Assignment
-
+/*
 let country = "Sri Lanka";
 let continent = "Asia";
 let populaiton = "17bil";
@@ -19,5 +21,28 @@ let populaiton = "17bil";
 console.log(country);
 console.log(continent);
 console.log(populaiton);
+*/
 
 // 11. Data Types
+
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof JavascriptIsFun);
+console.log(typeof 22);
+console.log(typeof "Malinda");
+
+JavascriptIsFun = "YES!";
+console.log(JavascriptIsFun);
+console.log(typeof JavascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2001;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
