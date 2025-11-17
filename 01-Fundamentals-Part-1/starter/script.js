72a9c669318a0345ef830fe270c8cafd26003b13
Malinda <greshanamarakoon@gmail.com>
2025-11-17 13:01:38 +0530

    lesson_9
    values and variables
    practice assg for that is done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,23 @@
+// 9. Values and Variables
 let js = "amazing";
-if (js === "amazing") alert("js is fun");
-
 console.log(40 + 20 + 10);
+
+console.log("Malinda1");
+console.log(25);
+
+let firstname = "Malinda";
+let lastname = "Amarakoon";
+
+console.log(firstname + " " + lastname);
+
+///// 10. Practice Assignment
+
+let country = "Sri Lanka";
+let continent = "Asia";
+let populaiton = "17bil";
+
+console.log(country);
+console.log(continent);
+console.log(populaiton);
+
+// 11. Data Types
