01f3d7d85555f9a2be7ee0d3cfe61ff6e8738608
Malinda <greshanamarakoon@gmail.com>
2026-01-29 12:48:11 +0530

    section 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "Audio"; // will throw an error
+// const private = 333; // will throw an error
