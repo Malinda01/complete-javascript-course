9ebf2253b423569ef9c464fd9428dc4a400f4d55
Malinda <greshanamarakoon@gmail.com>
2026-02-01 16:01:49 +0530

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,5 @@
+// 33. Activating strict mode
+/*
 "use strict";
 
 let hasDriversLicense = false;
@@ -8,3 +10,29 @@ if (hasDriversLicense) console.log("I can drive");
 
 // const interface = "Audio"; // will throw an error
 // const private = 333; // will throw an error
+*/
+
+// 34. Functions //
+/*
+// No parameter
+function logger() {
+  console.log("My name is Malinda");
+}
+
+logger(); // function invoking, calling, running
+logger();
+
+// With parameters
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples, ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor(0, 5);
+console.log(orangeJuice);
+*/
+
+// 35. Function Declaration vs Expression
