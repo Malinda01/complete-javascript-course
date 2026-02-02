a588b030212132726208d9e1b5a84f8ea332c4df
Malinda <greshanamarakoon@gmail.com>
2026-02-02 17:09:50 +0530

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -94,7 +94,7 @@ console.log(fruitProcessor(2, 3));
 */
 
 // 38. Reviewving function
-
+/*
 const calcAge = function (year) {
   return 2026 - year;
 };
@@ -115,3 +115,103 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(2001, "Malinda"));
 console.log(yearsUntilRetirement(1970, "Saman"));
 console.log(yearsUntilRetirement(1800, "Sri Wickrama Rajasinghe"));
+*/
+
+// 40. Introduction to Arrays
+/*
+const friend1 = "Malinda";
+const friend2 = "Greshan";
+
+const friends = ["Malinda", "Pathushi", "Steven", "Peter"];
+console.log(friends);
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+// Replace an item
+friends[2] = "Grey";
+console.log(friends);
+
+console.log("\n");
+
+const year = new Array(2001, 2002, 2003);
+console.log(year);
+
+const fName = "Malinda";
+const malinda = [fName, "Amarakoon", 2026 - 2001, friends];
+console.log(malinda);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2026 - birthYear;
+};
+const years = [2001, 2002, 2003, 2004, 2005];
+// console.log(calcAge(years)); - NaN
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+
+console.log(`Ages are, ${age1}, ${age2}, ${age3}`);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])];
+console.log(ages);
+*/
+
+// 41. Basic Array Operation
+/*
+const friends = ["Malinda", "Pathushi", "Steven", "Peter"];
+
+// Add - at end
+const newLength = friends.push("Jacobs", "Json");
+console.log(friends);
+console.log(newLength);
+
+// Add - at begin
+friends.unshift("Stathon", "Clarissa");
+console.log(friends);
+
+// Remove - last
+const popped = friends.pop();
+console.log(friends);
+console.log(popped);
+
+// Remove - first
+friends.shift();
+console.log(friends);
+
+// Index of an element
+console.log(friends.indexOf("John"));
+
+console.log(friends.includes("Malinda"));
+
+if (friends.includes("Malinda")) {
+  console.log("You have a friend!!");
+}
+*/
+// Coding excercise
+/* Write your code below. Good luck! 🙂 */
+/*
+const calcTip = function (bill) {
+  let tip_val = 0;
+
+  if (bill >= 50 && bill <= 300) {
+    tip_val = (bill * 15) / 100;
+  } else {
+    tip_val = (bill * 20) / 100;
+  }
+
+  return tip_val;
+};
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(`Bills are: ${bills}`);
+console.log(`Tips are: ${tips}`);
+console.log(`Total Bills are: ${totals}`);
+*/
+
+// 43. Introduction to objects
